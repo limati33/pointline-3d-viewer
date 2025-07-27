c6f7a3389589fb8b0f40b8f89d0f01dbe709fdf1
limon3089 <mannaftamirlan1@gmail.com>
2025-07-27 17:33:58 +0500

    Nothing works. Just commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import asyncio
-import platform
 import pygame
+import platform
 from config import WIDTH, HEIGHT, FPS
 from input_handler import handle_input
 from render import draw_scene
@@ -9,15 +8,14 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("3D Projection with Rotation")
 font = pygame.font.SysFont("arial", 20)
+clock = pygame.time.Clock()
 
-async def main():
+def main():
     while True:
         handle_input()
         draw_scene(screen, font)
-        await asyncio.sleep(1 / FPS)
+        pygame.display.flip()
+        clock.tick(FPS)
 
-if platform.system() == "Emscripten":
-    asyncio.ensure_future(main())
-else:
-    if __name__ == "__main__":
-        asyncio.run(main())
+if __name__ == "__main__":
+    main()
